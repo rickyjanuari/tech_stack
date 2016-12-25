b8ac7d47930a36df1191a6147f42147e1e05cd54
Ricky Januari <ricky010193@gmail.com>
2016-12-25 20:26:58 +0700

    commit dull

---
+++ index.ios.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+import { Provide } from 'react-redux';
+import { createStore } from 'redux';
+
+AppRegistry.registerComponent('tech_stack', () => App )
