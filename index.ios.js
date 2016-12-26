f2c997ab4c05eb84b8f131ef321d3254553f4adc
Ricky Januari <ricky010193@gmail.com>
2016-12-26 14:33:03 +0700

    commit

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
-import { Provide } from 'react-redux';
-import { createStore } from 'redux';
+
 
 AppRegistry.registerComponent('tech_stack', () => App )
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,12 @@
 import { combineReducers } from 'redux';
+import LibraryReducers from './LibraryReducers';
+import SelectionReducer from './SelectionReducer';
+
 
 export default combineReducers({
-
+	libraries: LibraryReducers,
+	selectedLibraryId: SelectionReducer
 });
+
+// console.log(store.getState());
+// { libraires: []}
